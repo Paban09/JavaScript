cc342ff36198f2c8a4c11f3ad160a334a261c777
Paban09 <masudranapaban22@gmail.com>
2022-04-18 23:50:45 +0600

    js

---
+++ 01. file1.js
@@ -0,0 +1,27 @@
+
+"use strict"; //helps to declare variable
+//print method
+console.log("Hello World");
+
+//declare variable
+var firstName="Masud"
+console.log(firstName)
+
+//assign value in variable
+firstName="Paban"
+console.log(firstName)
+
+//declare varibale by let
+
+let lastName="Rana"
+console.log(lastName)
+
+lastName="Pabon"
+console.log(lastName)
+
+//consrant value
+const pi=3.1416;
+console.log(pi);
+console.log(pi**2);
+
+
---
+++ 02. string.js
@@ -0,0 +1,23 @@
+//getting a string
+let firstName="Pabon";
+
+//printing the value in the index
+console.log(firstName[2]);
+
+//to print the lenbgth of a string
+console.log(firstName.length);
+
+//use of trim() method
+let lastName="    Rana   "
+console.log(lastName);
+console.log(lastName.length);
+
+lastName=lastName.trim();
+console.log(lastName);
+console.log(lastName.length);
+
+//to upper or lowerCase a string
+
+let newLastName=lastName.toUpperCase();
+console.log(newLastName)
+
